b33a6466a02b78fb4742c4f41d39670dd7b87fba
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-03-28 14:34:01 +0800

    NvChad to LazyVim + additional plugins for neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.cmd("set number")
+vim.cmd("highlight Normal guibg=NONE ctermbg=NONE")
--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-  { "ntk148v/habamax.nvim" },
+  { "slugbyte/lackluster.nvim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "habamax",
+      colorscheme = "lackluster",
     },
   },
 }
