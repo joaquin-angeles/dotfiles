33bcb2b2dc220057f9edc75e01c94f54ecdd25c6
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-03-31 17:29:28 +0800

    Fixed inconsistent hyprland cursors.

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -12,3 +12,9 @@ map('n', '<C-p>', ':Neotree filesystem reveal left<CR>', {})
 map('n', '<C-e>', require('telescope.builtin').find_files, { desc = 'Find Files' })
 
 vim.api.nvim_set_keymap("n", "H", ":Neotree toggle hidden<CR>", { noremap = true, silent = true })
+
+-- Keybinds for switching focus in normal mode only
+vim.keymap.set("n", "<S-h>", "<C-w>h", { desc = "Focus Left" })
+vim.keymap.set("n", "<S-j>", "<C-w>j", { desc = "Focus Down" })
+vim.keymap.set("n", "<S-k>", "<C-w>k", { desc = "Focus Up" })
+vim.keymap.set("n", "<S-l>", "<C-w>l", { desc = "Focus Right" })
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -5,3 +5,10 @@ vim.opt.number = true
 vim.opt.cursorline = true
 vim.cmd("hi CursorLine guibg=#242424 ctermbg=NONE")
 vim.cmd("hi CursorLineNr guifg=#eeeeee gui=bold ctermfg=255 cterm=bold")
+vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", { fg = "#708090" })
+vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", { fg = "#708090" })
+vim.api.nvim_set_hl(0, "NeoTreeFileNameOpened", { fg = "#708090", bold = true })
+vim.api.nvim_set_hl(0, "NeoTreeFileNameOpened", { fg = "#eeeeee", bold = true })
+vim.api.nvim_set_hl(0, "NeoTreeCursorLine", { fg = "#1c1c1c", bg = "#708090" })
+vim.api.nvim_set_hl(0, "NeoTreeCursorLineFile", { fg = "#1c1c1c", bg = "#eeeeee" })
+vim.api.nvim_set_hl(0, "NeoTreeFileIcon", { fg = "#eeeeee" })
