57d12312d4e639f1abd586fa7cc91c5fb55aa834
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-03-31 12:46:25 +0800

    Few changes.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,6 +3,5 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.fillchars = { eob = " " }
 vim.opt.number = true
 vim.opt.cursorline = true
-
-
-
+vim.cmd("hi CursorLine guibg=#242424 ctermbg=NONE")
+vim.cmd("hi CursorLineNr guifg=#eeeeee ctermfg=NONE")
