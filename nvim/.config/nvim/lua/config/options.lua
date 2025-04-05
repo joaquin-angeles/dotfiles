4b9ff4fd66c8cbae48aa5281ff5a4c7c91f627f7
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-04-05 18:12:21 +0800

    Completion config on Neovim replaced by the completion config made by TJ DeVries.

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -6,4 +6,12 @@ vim.opt.cursorline = true
 vim.cmd("hi CursorLine guibg=#242424 ctermbg=NONE")
 vim.cmd("hi CursorLineNr guifg=#eeeeee gui=bold ctermfg=255 cterm=bold")
 
-vim.opt.cmdheight = 0
+-- Function to check if the buffer is empty
+local function is_empty_buffer()
+  return vim.fn.line('$') == 1 and vim.fn.getline(1) == ''
+end
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
--- nvim/.config/nvim/lua/config/theme.lua
+++ nvim/.config/nvim/lua/config/theme.lua
@@ -1,34 +1,42 @@
-for _, group in ipairs({
-  "Normal", "NonText", "EndOfBuffer", "NormalFloat", "FloatBorder",
-  "WinSeparator", "TelescopeNormal", "TelescopeBorder",
-  "TelescopePromptNormal", "TelescopePromptBorder",
-  "TelescopeResultsNormal", "TelescopeResultsBorder",
-  "TelescopePreviewNormal", "TelescopePreviewBorder"
-}) do
-  vim.api.nvim_set_hl(0, group, { bg = "NONE" })
-end
+-- Set the background of Neovim to transparent
+vim.cmd("hi Normal guibg=NONE ctermbg=NONE")
+vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
 
-vim.cmd([[
-  highlight Normal guibg=NONE ctermbg=NONE
-  highlight NormalFloat guibg=NONE ctermbg=NONE
-  highlight FloatBorder guibg=NONE ctermbg=NONE
-  highlight NoiceCmdlinePopup guibg=NONE ctermbg=NONE
-  highlight NoiceCmdlinePopupBorder guibg=NONE ctermbg=NONE
-]])
+-- For plugins with floating windows or popups
+vim.cmd("hi NormalFloat guibg=NONE ctermbg=NONE")
+vim.cmd("hi FloatBorder guibg=NONE ctermbg=NONE")
+vim.cmd("hi Pmenu guibg=NONE ctermbg=NONE")
+vim.cmd("hi PmenuSel guibg=NONE ctermbg=NONE")
 
--- Set background of cmdline popup elements to NONE
-vim.cmd("hi NoiceCmdlinePopup guibg=NONE")
-vim.cmd("hi NoiceCmdlinePopupBorder guibg=NONE")
-vim.cmd("hi NoiceCmdlinePrompt guibg=NONE")
-vim.cmd("hi NoiceCmdlineIcon guibg=NONE")
+-- Specific plugin UIs
+vim.cmd("hi TelescopeNormal guibg=NONE ctermbg=NONE")
+vim.cmd("hi TelescopeBorder guibg=NONE ctermbg=NONE")
+vim.cmd("hi TelescopePromptNormal guibg=NONE ctermbg=NONE")
+vim.cmd("hi TelescopePromptBorder guibg=NONE ctermbg=NONE")
+vim.cmd("hi TelescopePromptTitle guibg=NONE ctermbg=NONE")
+vim.cmd("hi TelescopeResultsNormal guibg=NONE ctermbg=NONE")
+vim.cmd("hi TelescopeResultsBorder guibg=NONE ctermbg=NONE")
+vim.cmd("hi TelescopeResultsTitle guibg=NONE ctermbg=NONE")
+vim.cmd("hi TelescopePreviewNormal guibg=NONE ctermbg=NONE")
+vim.cmd("hi TelescopePreviewBorder guibg=NONE ctermbg=NONE")
+vim.cmd("hi TelescopePreviewTitle guibg=NONE ctermbg=NONE")
 
--- Set background of messages popup elements to NONE
+-- Noice plugin UI transparency
+vim.cmd("hi NoicePopup guibg=NONE ctermbg=NONE")
+vim.cmd("hi NoicePopupBorder guibg=NONE ctermbg=NONE")
+vim.cmd("hi NoiceMessage guibg=NONE ctermbg=NONE")
 vim.cmd("hi NoiceMessageView guibg=NONE")
 vim.cmd("hi NoiceMessageViewBorder guibg=NONE")
+vim.cmd("hi NoiceCmdline guibg=NONE ctermbg=NONE")
+vim.cmd("hi NoiceCmdlinePrompt guibg=NONE")
+vim.cmd("hi NoiceCmdlineIcon guibg=NONE")
+vim.cmd("hi NoiceCmdlinePopup guibg=NONE ctermbg=NONE")
+vim.cmd("hi NoiceCmdlinePopupBorder guibg=NONE ctermbg=NONE")
 
--- Set background of popupmenu elements to NONE
-vim.cmd("hi NoicePopupmenu guibg=NONE")
-vim.cmd("hi NoicePopupmenuBorder guibg=NONE")
-vim.cmd("hi NoicePopupmenuMatch guibg=NONE")
-vim.cmd("hi NoicePopupmenuSelected guibg=NONE")
+-- UndoTree plugin transparency
+vim.cmd("hi UndoTreeNormal guibg=NONE ctermbg=NONE")
+
+-- Pmenu transparency for completion menu
+vim.cmd("hi Pmenu guibg=NONE ctermbg=NONE")
+vim.cmd("hi PmenuSel guibg=NONE ctermbg=NONE")
 
