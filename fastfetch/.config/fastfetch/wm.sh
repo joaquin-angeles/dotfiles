6343690cd7b3d9a5893eda31e4c03e8bb95922e4
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-04-08 16:12:04 +0800

    Waybar changes. Window title removed.

--- fastfetch/.config/fastfetch/wm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
-  echo ""
-else
-  echo ""
-fi
--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,6 @@ return {
     'williamboman/mason-lspconfig.nvim',
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { 'lua_ls' }
       })
     end
   },
