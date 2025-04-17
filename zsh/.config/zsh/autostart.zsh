f731b432ed6fb94cd52d15c7eae1829bf62c7c14
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-04-17 18:32:26 +0800

    Kitty config has active padding

--- zsh/.config/zsh/autostart.zsh
+++ zsh/.config/zsh/autostart.zsh
@@ -1,7 +1,7 @@
 # Kitty configurations
-if [[ $TERM == "xterm-kitty" ]]; then
-  kitty @ set-spacing padding=6
-fi
+# if [[ $TERM == "xterm-kitty" ]]; then
+#   kitty @ set-spacing padding=6
+# fi
 
 if [[ $TERM == "xterm-kitty" ]]; then
     fastfetch --kitty-direct ~/.config/fastfetch/logo.png
