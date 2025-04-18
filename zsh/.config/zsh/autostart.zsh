98b17d1cb5a7c061730a0f06fe417650050dae1d
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-04-18 18:27:34 +0800

    Removed fastfetch on terminal startup.

--- zsh/.config/zsh/autostart.zsh
+++ zsh/.config/zsh/autostart.zsh
@@ -1,14 +1,3 @@
-# Kitty configurations
-# if [[ $TERM == "xterm-kitty" ]]; then
-#   kitty @ set-spacing padding=6
-# fi
-
-if [[ $TERM == "xterm-kitty" ]]; then
-    fastfetch --kitty-direct ~/.config/fastfetch/logo.png
-else
-    fastfetch --logo ~/.config/fastfetch/logo.txt
-fi
-
 # Autoload
 autoload -U compinit && compinit
 
