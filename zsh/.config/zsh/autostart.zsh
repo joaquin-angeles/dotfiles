6565473662b32193fb9c4dfce81430c539551bd8
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-04-21 22:13:39 +0800

    Excluded a few commands from the terminal window title.

--- zsh/.config/zsh/autostart.zsh
+++ zsh/.config/zsh/autostart.zsh
@@ -4,22 +4,25 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
 fi
 
 # Window titles
+excluded_commands=("git commit" "git add" "clear" "c" "ls" "pwd" "exit" "history")
 function set_window_title() {
   local cmd="$1"
   local dir="${PWD/#$HOME/\~}"  # Replace $HOME with ~
+  for excluded in "${excluded_commands[@]}"; do
+    if [[ "$cmd" == "$excluded"* ]]; then
+      return 0  # Skip updating the title if it's an excluded command
+    fi
+  done
   if [[ -n "$cmd" ]]; then
-    # Show window title with the command executed
     print -Pn "\e]0;${USER}@${HOST}:${dir} (${cmd})\a"
   else
     # Show window title without parentheses when idle
     print -Pn "\e]0;${USER}@${HOST}:${dir}\a"
   fi
 }
-
 preexec() {
   set_window_title "$1"
 }
-
 precmd() {
   set_window_title ""
 }
