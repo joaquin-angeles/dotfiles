d7cfd4a80247199e45f00a71a90d6d7b077f1c86
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-04-21 22:10:59 +0800

    Window titles for terminals look better.

--- zsh/.config/zsh/autostart.zsh
+++ zsh/.config/zsh/autostart.zsh
@@ -4,8 +4,24 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
 fi
 
 # Window titles
+function set_window_title() {
+  local cmd="$1"
+  local dir="${PWD/#$HOME/\~}"  # Replace $HOME with ~
+  if [[ -n "$cmd" ]]; then
+    # Show window title with the command executed
+    print -Pn "\e]0;${USER}@${HOST}:${dir} (${cmd})\a"
+  else
+    # Show window title without parentheses when idle
+    print -Pn "\e]0;${USER}@${HOST}:${dir}\a"
+  fi
+}
+
+preexec() {
+  set_window_title "$1"
+}
+
 precmd() {
-  print -Pn "\e]0;%n@%m:%~\a"
+  set_window_title ""
 }
 
 # Autoload
