e7fb48feb22e23ab74506aafd71710c424a293ab
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-04-21 18:28:48 +0800

    Commited random changes.

--- zsh/.config/zsh/autostart.zsh
+++ zsh/.config/zsh/autostart.zsh
@@ -1,22 +1,11 @@
+# P10K Instant Prompt
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 # Autoload
 autoload -U compinit && compinit
 
-# New line after commands
-typeset -g __has_prompted_once=false
-
-precmd() {
-    if [[ $__has_prompted_once == false ]]; then
-        __has_prompted_once=true
-        return
-    fi
-
-    local last_command=$(fc -ln -1)
-    if [[ "$last_command" != "clear" && "$last_command" != "c" ]]; then
-        echo
-    fi
-}
-
 # Shell integrations
 eval "$(fzf --zsh)"
-eval "$(oh-my-posh init zsh --config '~/.config/zsh/omp.toml')"
 eval "$(zoxide init zsh)"
--- zsh/.config/zsh/plugins.zsh
+++ zsh/.config/zsh/plugins.zsh
@@ -11,6 +11,10 @@ zinit snippet OMZP::archlinux
 zinit snippet OMZP::command-not-found
 
 # Plugins (Zinit)
+zinit light romkatv/powerlevel10k # P10K
 zinit light zsh-users/zsh-syntax-highlighting # Syntax highlighting
 zinit light zsh-users/zsh-completions # Autocompletion
 zinit light zsh-users/zsh-autosuggestions # Suggesting past commands 
+
+# Configurations
+[[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh # P10K
