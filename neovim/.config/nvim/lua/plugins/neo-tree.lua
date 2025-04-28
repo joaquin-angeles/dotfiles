963281a7c1607277730a503e51925442779b0592
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-04-28 15:48:47 +0800

    Replaced kitty with foot and changed the image preview on lf to chafa.

--- neovim/.config/nvim/lua/plugins/neo-tree.lua
+++ neovim/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,18 @@
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons',
-  },
-  config = function()
+    'nvim-neo-tree/neo-tree.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
         require("neo-tree").setup({
+            popup_border_style = "rounded",
             close_if_last_window = true,
             sources = { "filesystem", "buffers", "git_status" },
+            window = {
+                position = "float",
+                hide_cursor = true,
+            },
             filesystem = {
                 follow_current_file = { enabled = true },
                 hijack_netrw_behavior = "open_default",
@@ -20,6 +25,7 @@ return {
                     mappings = {
                         ["."] = "toggle_hidden",
                     },
+                    hide_cursor = true,
                 },
             },
             default_component_configs = {
