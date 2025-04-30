4fb77f80ecc2a927f00ba03e74b2f1805a895868
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-04-30 22:09:04 +0800

    Minor changes to waybar.

--- neovim/.config/nvim/lua/colors/syntax.lua
+++ neovim/.config/nvim/lua/colors/syntax.lua
@@ -1,77 +1,64 @@
 local set = vim.api.nvim_set_hl
+
 local colors = {
     red            = "#865054",
-    bright_red     = "#976061",
-    orange         = "#926e3f",
     green          = "#5f7a5f",
-    bright_green   = "#729a72",
     yellow         = "#a4965f",
-    bright_yellow  = "#a49a72",
     blue           = "#5f7a8c",
-    bright_blue    = "#72949a",
     magenta        = "#75607a",
-    bright_magenta = "#8c729a",
     cyan           = "#5f7a7f",
+    orange         = "#c97c3e",
+    bright_red     = "#b85757",
+    bright_green   = "#729a72",
+    bright_yellow  = "#a49a72",
+    bright_blue    = "#72949a",
+    bright_magenta = "#8c729a",
     bright_cyan    = "#729a96",
-    black          = "#1c1c1c",
-    bright_black   = "#444444"
+    bright_orange  = "#8f5a33",
+    bright_black   = "#404040",
+    bright_gray    = "#505050",
+    fg             = "#eeeeee"
 }
 
--- Treesitter highlights
-local ts_highlights = {
-    -- Functions, methods, and identifiers
-    ['@function'] = { fg = colors.blue },
-    ['@function.builtin'] = { fg = colors.bright_blue },
-    ['@method'] = { fg = colors.blue },
-    ['@method.call'] = { fg = colors.bright_blue },
+-- Typical highlight groups
+set(0, "Comment",      { fg = colors.bright_black, italic = true })
+set(0, "String",       { fg = colors.green })
+set(0, "Function",     { fg = colors.blue })
+set(0, "Keyword",      { fg = colors.bright_orange, bold = true, italic = true })
+set(0, "Float",        { fg = colors.yellow })
+set(0, "Identifier",   { fg = colors.red })
+set(0, "Number",       { fg = colors.bright_yellow })
+set(0, "Boolean",      { fg = colors.red })
+set(0, "Type",         { fg = colors.green })
+set(0, "Operator",     { fg = colors.red })
+set(0, "Constant",     { fg = colors.yellow })
+set(0, "PreProc",      { fg = colors.yellow })
+set(0, "Title",        { fg = colors.orange })
 
-    -- Types and classes
-    ['@type'] = { fg = colors.cyan },
-    ['@class'] = { fg = colors.magenta },
-    ['@interface'] = { fg = colors.blue },
-
-    -- Keywords and operators
-    ['@keyword'] = { fg = colors.orange },
-    ['@keyword.operator'] = { fg = colors.orange },
-
-    -- Comments and strings
-    ['@comment'] = { fg = colors.bright_black, italic = true },
-    ['@string'] = { fg = colors.magenta },
-    ['@string.regex'] = { fg = colors.bright_magenta },
-
-    -- Numbers and constants
-    ['@number'] = { fg = colors.bright_yellow },
-    ['@boolean'] = { fg = colors.bright_yellow },
-    ['@constant'] = { fg = colors.yellow },
-
-    -- Punctuation and operators
-    ['@operator'] = { fg = colors.red },
-    ['@punctuation'] = { fg = colors.bright_green },
-
-    -- Error highlighting
-    ['@error'] = { fg = colors.bright_red },
-
-    -- Types and qualifiers
-    ['@type.qualifier'] = { fg = colors.bright_cyan },
-
-    -- Add custom highlight for specific keywords
-    ['@keyword.local'] = { fg = colors.orange, italic = true },
-    ['@keyword.source'] = { fg = colors.orange, italic = true },
-}
-
--- Syntax highlights (regular Vim syntax highlighting)
-set(0, 'Keyword', { fg = colors.orange })
-set(0, 'Function', { fg = colors.blue })
-set(0, 'Identifier', { fg = colors.green })
-set(0, 'Type', { fg = colors.cyan })
-set(0, 'String', { fg = colors.magenta })
-set(0, 'Comment', { fg = colors.bright_black, italic = true })
-set(0, 'Number', { fg = colors.bright_yellow })
-set(0, 'Error', { fg = colors.bright_red, bg = colors.bright_black, bold = true })
-set(0, 'Constant', { fg = colors.yellow })
-set(0, 'Variable', { fg = colors.green })
-
--- Apply Treesitter highlights
-for group, hl in pairs(ts_highlights) do
-    set(0, group, hl)
-end
+-- Treesitter highlight groups
+set(0, "@comment",             { link = "Comment" })
+set(0, "@string",              { link = "String" })
+set(0, "@number",              { link = "Number" })
+set(0, "@boolean",             { link = "Boolean" })
+set(0, "@float",               { link = "Number" })
+set(0, "@keyword",             { link = "Keyword" })
+set(0, "@keyword.return",      { link = "Keyword" })
+set(0, "@operator",            { link = "Operator" })
+set(0, "@function",            { link = "Function" })
+set(0, "@function.builtin",    { fg = colors.bright_blue, bold = true })
+set(0, "@method",              { link = "Function" })
+set(0, "@constructor",         { fg = colors.yellow })
+set(0, "@variable",            { fg = colors.fg })
+set(0, "@variable.builtin",    { fg = colors.red, italic = true })
+set(0, "@type",                { link = "Type" })
+set(0, "@type.builtin",        { fg = colors.cyan, italic = true })
+set(0, "@class",               { fg = colors.yellow })
+set(0, "@interface",           { fg = colors.bright_blue })
+set(0, "@property",            { fg = colors.magenta })
+set(0, "@field",               { fg = colors.green })
+set(0, "@punctuation",         { fg = colors.bright_black })
+set(0, "@punctuation.delimiter", { fg = colors.bright_black })
+set(0, "@punctuation.bracket", { fg = colors.bright_black })
+set(0, "@tag",                 { fg = colors.red })
+set(0, "@tag.attribute",       { fg = colors.yellow })
+set(0, "@tag.delimiter",       { fg = colors.bright_black })
