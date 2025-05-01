75a89f19e3368780649660c63c1a4b3e251983c8
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-05-01 16:02:42 +0800

    Added vi to alias for nvim

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -4,13 +4,14 @@ cd() {
     z "$@" && eza
 }
 
-alias cat='bat'
+alias cat=bat
 alias lf='sh ~/.config/lf/lfrun'
-alias ff='fastfetch'
-alias grep='rg'
+alias ff=fastfetch
+alias grep=rg
 alias paru='paru --skipreview'
-alias k='kill'
-alias ls='eza'
-alias pk='pkill'
+alias k=kill
+alias ls=eza
+alias pk=pkill
 alias flatpak='flatpak --user'
 alias vim=nvim
+alias vi=nvim
