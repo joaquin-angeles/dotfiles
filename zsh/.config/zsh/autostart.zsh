7fdb27785aa42e877972a7c88b6fab0dc14050fe
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-05-01 10:10:16 +0800

    Added fastfetch to zsh start up.

--- zsh/.config/zsh/autostart.zsh
+++ zsh/.config/zsh/autostart.zsh
@@ -1,3 +1,6 @@
+fastfetch
+echo ""
+
 # P10K Instant Prompt
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
     source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
