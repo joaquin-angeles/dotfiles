f9066c508bc4299b74e36260b535100ece12b272
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-05-03 17:56:00 +0800

    Changed padding on waybar.

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -35,6 +35,7 @@ return {
                     'filetype'
                 },
                 lualine_y = {
+                    '%p%%',
                     'location'
                 },
                 lualine_z = {
