dd2f4243f76a4fbec68ad7ec64abdaa06260c8d1
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-05-17 15:26:46 +0800

    Removed yazi and lf file managers to use neotree again.

--- neovim/.config/nvim/lua/colors/transparency.lua
+++ neovim/.config/nvim/lua/colors/transparency.lua
@@ -27,3 +27,12 @@ vim.api.nvim_set_hl(0, 'NoiceCmdlinePrompt', { bg = nil })
 vim.api.nvim_set_hl(0, 'NoiceCmdlineIcon', { bg = nil })
 vim.api.nvim_set_hl(0, 'NoiceCmdlinePopup', { bg = nil, cterm = { bg = nil } })
 vim.api.nvim_set_hl(0, 'NoiceCmdlinePopupBorder', { bg = nil, cterm = { bg = nil } })
+
+-- Neotree
+vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", { fg = "#5f7a7f" })
+vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", { fg = "#5f7a7f" })
+vim.api.nvim_set_hl(0, "NeoTreeFileNameOpened", { fg = "#5f7a7f", bold = true })
+vim.api.nvim_set_hl(0, "NeoTreeFileNameOpened", { fg = "#eeeeee", bold = true })
+vim.api.nvim_set_hl(0, "NeoTreeCursorLine", { fg = "#7a9e34", bg = "NONE" })
+vim.api.nvim_set_hl(0, "NeoTreeCursorLineFile", { fg = "#1c1c1c", bg = "#eeeeee" })
+vim.api.nvim_set_hl(0, "NeoTreeFileIcon", { fg = "#eeeeee" })
--- neovim/.config/nvim/lua/plugins/yazi.lua
+++ neovim/.config/nvim/lua/plugins/yazi.lua
@@ -1,42 +1,16 @@
 return {
-  "mikavilpas/yazi.nvim",
-  event = "VeryLazy",
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
   dependencies = {
-    -- check the installation instructions at
-    -- https://github.com/folke/snacks.nvim
-    "folke/snacks.nvim"
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
   },
-  keys = {
-    -- 👇 in this section, choose your own keymappings!
-    {
-      "<leader>f",
-      mode = { "n", "v" },
-      "<cmd>Yazi<cr>",
-      desc = "Open yazi at the current file",
-    },
-    {
-      -- Open in the current working directory
-      "<leader>e",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
-    },
-    {
-      "<c-up>",
-      "<cmd>Yazi toggle<cr>",
-      desc = "Resume the last yazi session",
-    },
-  },
-  ---@type YaziConfig | {}
+  lazy = false, -- neo-tree will lazily load itself
+  ---@module "neo-tree"
+  ---@type neotree.Config?
   opts = {
-    -- if you want to open yazi instead of netrw, see below for more info
-    open_for_directories = true,
-    keymaps = {
-      show_help = "<f1>",
-    },
+    -- fill any relevant options here
   },
-  -- 👇 if you use `open_for_directories=true`, this is recommended
-  init = function()
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-  end,
 }
