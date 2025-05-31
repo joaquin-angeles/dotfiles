9070ddd956203153517c895839fcd9d4d72e0468
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-05-31 14:34:49 +0800

    Tiny tweaks.

--- neovim/.config/nvim/lua/plugins/noice.lua
+++ neovim/.config/nvim/lua/plugins/noice.lua
@@ -1,35 +1,35 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    views = {
-      cmdline_popup = {
-        position = {
-          row = 1,
-          col = "50%",
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+        views = {
+            cmdline_popup = {
+                position = {
+                    row = 1,
+                    col = "50%",
+                },
+                size = {
+                    width = 60,
+                    height = "auto",
+                },
+                border = {
+                    style = "rounded",
+                },
+                win_options = {
+                    winblend = 0, -- Ensure full opacity
+                    winhighlight = "NormalFloat:Normal,FloatBorder:Normal",
+                },
+            },
         },
-        size = {
-          width = 60,
-          height = "auto",
+        presets = {
+            bottom_search = true,
+            command_palette = true,
+            long_message_to_split = true,
         },
-        border = {
-          style = "rounded",
+        routes = {
         },
-        win_options = {
-          winblend = 0, -- Ensure full opacity
-          winhighlight = "NormalFloat:Normal,FloatBorder:Normal",
-        },
-      },
     },
-    presets = {
-      bottom_search = true,
-      command_palette = true,
-      long_message_to_split = true,
+    dependencies = {
+        "MunifTanjim/nui.nvim",
     },
-    routes = {
-    },
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-  },
 }
