76debe7f788dc6b393404a63e2622c9461894496
joaquin-angeles <selegnaniuqaoj@gmail.com>
2025-07-30 19:08:03 +0800

    Changed some neovim stuff.

--- neovim/.config/nvim/lua/plugins/lualine.lua
+++ neovim/.config/nvim/lua/plugins/lualine.lua
@@ -6,35 +6,34 @@ return {
             options = {
                 icons_enabled = false,
                 disabled_filetypes = { 'alpha', 'neo-tree' },
-                section_separators = { left = '', right = '' },
                 component_separators = { left = '::', right = '::' },
                 theme = {
                     normal = {
-                        a = { fg = '#d3d3d5', bg = '#323232' },
-                        b = { fg = '#d3d3d5', bg = '#252525' },
+                        a = { fg = '#dfdfdf', bg = '#323232' },
+                        b = { fg = '#dfdfdf', bg = '#252525' },
                         c = { bg = 'NONE' }
                     },
                     insert = {
-                        a = { fg = '#1c1c1c', bg = '#5f7a5f' },
-                        b = { fg = '#d3d3d5', bg = '#252525' },
+                        a = { fg = '#dfdfdf', bg = '#323232' },
+                        b = { fg = '#dfdfdf', bg = '#252525' },
                         c = { bg = 'NONE' },
                     },
 
                     visual = {
-                        a = { fg = '#1c1c1c', bg = '#7a8a4f' },
-                        b = { fg = '#d3d3d5', bg = '#252525' },
+                        a = { fg = '#dfdfdf', bg = '#323232' },
+                        b = { fg = '#dfdfdf', bg = '#252525' },
                         c = { bg = 'NONE' },
                     },
 
                     replace = {
-                        a = { fg = '#1c1c1c', bg = '#865054' },
-                        b = { fg = '#d3d3d5', bg = '#252525' },
+                        a = { fg = '#dfdfdf', bg = '#323232' },
+                        b = { fg = '#dfdfdf', bg = '#252525' },
                         c = { bg = 'NONE' },
                     },
 
                     command = {
-                        a = { fg = '#1c1c1c', bg = '#a4965f' },
-                        b = { fg = '#d3d3d5', bg = '#252525' },
+                        a = { fg = '#dfdfdf', bg = '#323232' },
+                        b = { fg = '#dfdfdf', bg = '#252525' },
                         c = { bg = 'NONE' },
                     },
 
@@ -49,7 +48,7 @@ return {
                 lualine_a = {
                     {
                         'mode',
-                        separator = { left = '', right = '' }
+                        separator = { left = '', right = '' }
                     }
                 },
                 lualine_b = {
@@ -68,7 +67,7 @@ return {
                 lualine_z = {
                     {
                         'location',
-                        separator = { left = '', right = '' }
+                        separator = { left = '', right = '' }
                     }
                 }
             },
