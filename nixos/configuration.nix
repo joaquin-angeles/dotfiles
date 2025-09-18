e9481dd6fe7c57e796018a46531c498ad6118785
Joaquin Angeles <selegnaniuqaoj@gmail.com>
2025-09-18 19:35:56 +0800

    Added some security to NixOS install

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
+    # Modules
     imports = [
         ./modules/development-tools.nix
         ./modules/fonts.nix
@@ -19,6 +20,41 @@
     # Networking options
     networking.hostName = "nixos"; # Define your hostname.
     networking.wireless.iwd.enable = true;
+    # Fail2Ban
+    services.fail2ban = {
+        enable = true;
+        maxretry = 3;
+        ignoreIP = [
+            "10.0.0.0/8" "172.16.0.0/12" "192.168.0.0/16"
+            "8.8.8.8"
+        ];
+        bantime = "30m";
+        bantime-increment = {
+            enable = true;
+            multipliers = "1 2 4 8 16 32 64";
+            maxtime = "24h";
+            overalljails = true;
+        };
+    };
+    # Firewall
+    networking.nftables.enable = true;
+    networking.nftables.ruleset = ''
+    table inet filter {
+      chain input {
+        iifname lo accept
+        ct state {established, related} accept
+        ip protocol icmp accept
+        ip6 nexthdr icmpv6 accept
+      }
+      chain output {
+        type filter hook output priority 0; policy accept;
+      }
+      chain forward {
+        type filter hook forward priority 0; policy drop;
+      }
+    }
+    '';
+    networking.nftables.checkRuleset = true;
 
     # Zram
     zramSwap = {
